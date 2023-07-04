857c5479fda80f48a19567e8995730b45241ba76
hocine699 <127848706+hocine699@users.noreply.github.com>
2023-07-04 16:31:17 +0200

    ss

---
+++ .github/workflows/main.tf
@@ -0,0 +1,33 @@
+terraform {
+  cloud {
+    organization = "hocinho699"
+
+    workspaces {
+      name = "TerraformCI"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+}
+
+resource "random_string" "uniquestring" {
+  length  = 20
+  special = false
+  upper   = false
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "Hocine-TD-1"
+  location = "eastus"
+}
+
+resource "azurerm_storage_account" "storageaccount" {
+  name                     = "stg${random_string.uniquestring.result}"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
